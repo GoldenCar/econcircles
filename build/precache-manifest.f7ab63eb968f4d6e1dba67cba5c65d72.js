dfe41af8500ef342d961d324facfe2bff366470d
goldencar <goldencar0822@gmail.com>
2020-06-23 19:19:31 +0300

    initial commit

---
+++ build/precache-manifest.f7ab63eb968f4d6e1dba67cba5c65d72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "c6cf87ff41a8c16fd4a5",
+    "url": "/static/js/main.c6cf87ff.chunk.js"
+  },
+  {
+    "revision": "c1704368832400b0ca9f",
+    "url": "/static/js/2.c1704368.chunk.js"
+  },
+  {
+    "revision": "c6cf87ff41a8c16fd4a5",
+    "url": "/static/css/main.8c6eef5b.chunk.css"
+  },
+  {
+    "revision": "85d79f74e086648bcfb57e9dc8e7bfde",
+    "url": "/index.html"
+  }
+];
